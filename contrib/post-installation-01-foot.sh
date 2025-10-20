9022b6d44f81830052718e204c2b047865cf9fe1
Johan Malm <jgm323@gmail.com>
2025-10-20 20:40:21 +0100

    post-installation-01-foot.sh: remove theme as not on trixie

--- contrib/post-installation-01-foot.sh
+++ contrib/post-installation-01-foot.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.config/foot
 
 cat > ~/.config/foot/foot.ini <<- EOF
 	[main]
-	include = /usr/share/foot/themes/apprentice
+#	include = /usr/share/foot/themes/apprentice
 	font = DeJaVu Sans Mono:size=10
 	initial-window-size-chars = 120x35
 
