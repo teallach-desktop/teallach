eb204131959172a18d2f3621e4f2e829e1168439
Johan Malm <jgm323@gmail.com>
2026-09-22 22:49:09 +0100

    Add post-installation-{02,03}...

---
+++ contrib/post-installation-02-kvantum.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/Kvantum
+
+cat > ~/.config/Kvantum/kvantum.kvconfig <<- EOF
+	[General]
+	theme=KvGnomeDark
+EOF
+
---
+++ contrib/post-installation-03-qt6ct.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.config/qt6ct
+
+cat > ~/.config/qt6ct/qt6ct.conf <<- EOF
+	[Appearance]
+	style=kvantum-dark
+EOF
+
